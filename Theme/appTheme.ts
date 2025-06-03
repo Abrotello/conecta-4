2340a49cb0c88c615d8104057880f96752b99065
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-02 21:41:28 -0600

    fix: dict to js obj

--- Theme/appTheme.ts
+++ Theme/appTheme.ts
@@ -1,9 +1,9 @@
 export const appColors = {
-    'main' : '#1A212B',
-    'secondary' : '#303B4A',
-    'player1' : '#BE3E3E',
-    'player2' : '#FFE977',
+    main : '#1A212B',
+    secondary : '#303B4A',
+    player1 : '#BE3E3E',
+    player2 : '#FFE977',
 
-    "opactiyPlayer1": "rgba(190, 62, 62, 0.7)",
-    "opactiyPlayer2": "rgba(255, 233, 119, 0.7)",
+    opactiyPlayer1: "rgba(190, 62, 62, 0.7)",
+    opactiyPlayer2: "rgba(255, 233, 119, 0.7)",
 }
